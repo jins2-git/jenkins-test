b43149918027e799e0b2200810af1ebbc83cc8fa
jins2 <jins2@adcapsule.co.kr>
2020-11-26 15:05:50 +0900

    Initial commit

---
+++ src/main/java/com/example/kpi/test/testController.java
@@ -0,0 +1,24 @@
+package com.example.kpi.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class testController {
+
+    @GetMapping("/kpi_test")
+    public String test(ModelMap model) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
+
+        Date today = new Date();
+        String now = sdf.format(today);
+
+        model.addAttribute("now", now);
+
+        return "test";
+    }
+}
